2c24fce3fdd13dee83553a59688fdfa56c0fa0d6
abhinav-selvaraj <abhipuli2579@gmail.com>
2023-11-08 01:04:16 -0800

    api setup

--- website/views.py
+++ website/views.py
@@ -1,7 +1,19 @@
 from flask import Blueprint
+import requests #API Import
+
+
+
+
+
+
 
 views = Blueprint('views', __name__)
 
 @views.route('/')
 def home():
-    return "<h1> Home Page <h1>"
+    url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&apikey=S1WA6956TYGOYFGH' #test
+    r = requests.get(url)
+    data = r.json()
+
+    #print(data)
+    return data
