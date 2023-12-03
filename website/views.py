462687bf9d7b4669f2841d49f86292319215bca8
abhinav-selvaraj <abhipuli2579@gmail.com>
2023-12-03 15:02:33 -0800

    added stock selection in table view

--- website/views.py
+++ website/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, jsonify
 import requests #API Import
 import json, time, requests
 from datetime import datetime
@@ -40,17 +40,22 @@ def homePage():
 
     return render_template('base.html' , stocks=symbols)
 
-@views.route('/TestAPI')
-def data():
+@views.route('/RawData/<symbol>')
+def data(symbol):
+    symbols = ['Select a stock', 'AAPL', 'AMZN', 'AVGO','BRK-B', 'GOOG', 'HD' , 'IBM', 'JNJ', 'JPM', 'LLY', 'MA', 'META', 'MSFT', 'NVDA', 'PG', 'TSLA','TSM','UNH', 'V', 'XOM' ] 
+
+    # Construct the file path based on the symbol parameter
+    filepath = f'CS_122_Final_Project/stockDataJsons/{symbol}_data.JSON'
     
+   
     
-    filepath = 'CS_122_Final_Project\stockDataJsons\IBM_data.json'
     with open(filepath, 'r') as file:
         contents = json.load(file)
         keyList = contents.keys()
         data = contents
-       
-    return render_template('TestAPI.html', data=data, keyList=keyList)
+
     
+
+    return render_template('TestAPI.html', symbols=symbols, data=data, keyList=keyList, symbol=symbol)
   
 
