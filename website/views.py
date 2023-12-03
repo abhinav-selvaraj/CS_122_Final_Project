2b17cb82860e708d5d50b68736a80ded62d9ba4f
abhinav-selvaraj <abhipuli2579@gmail.com>
2023-12-02 23:59:05 -0800

    Setup JSON files for top 25 stocks, and api calls now make and update the files at midnight every night, only if the home pahe is clicked

--- website/views.py
+++ website/views.py
@@ -1,23 +1,50 @@
 from flask import Blueprint, render_template
 import requests #API Import
-import json
-
+import json, time, requests
+from datetime import datetime
 
 views = Blueprint('views', __name__)
 
+
+
+
+
+
+
 @views.route('/')
 def homePage():
-   
+    symbols = ['AAPL', 'AMZN', 'AVGO','BRK-B', 'GOOG', 'HD' , 'IBM', 'JNJ', 'JPM', 'LLY', 'MA', 'META', 'MSFT', 'NVDA', 'PG', 'TSLA','TSM','UNH', 'V', 'XOM' ] 
+    api_key = 'S1WA6956TYGOYFGH'
+    current_time = datetime.now().time()
+    print(current_time)
+    if current_time.hour == 0 and current_time.minute == 0: #time for update
+        print("fetching data")
+        for symbol in symbols:
+            url = f'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&apikey={api_key}'
+            response = requests.get(url)
+            print(response.status_code)
+            if response.status_code == 200:
+                data = response.json()
+        
+            # Process the data as needed
+        
+            # Write the data to a new JSON file
+                with open(f'CS_122_Final_Project\stockDataJsons\{symbol}_data.JSON', 'w') as json_file:
+                    json.dump(data, json_file, indent=2)
+                    print(f"Data for {symbol} written to {symbol}_data.json")
+                
+            else:
+                print(f"Failed to fetch data for {symbol}. Status code: {response.status_code}")
+                return "failed"  
+      
 
-    return render_template('base.html')
+    return render_template('base.html' , stocks=symbols)
 
 @views.route('/TestAPI')
 def data():
-    #url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&apikey=S1WA6956TYGOYFGH' #test
-    #r = requests.get(url)
-    #data = r.json()
     
-    filepath = './IBM_TIME_SERIES_DATA.json'
+    
+    filepath = 'CS_122_Final_Project\stockDataJsons\IBM_data.json'
     with open(filepath, 'r') as file:
         contents = json.load(file)
         keyList = contents.keys()
