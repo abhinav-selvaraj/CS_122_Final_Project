800b6ac633b166e292ebc762e1bcc6d285e9c07b
abhinav-selvaraj <abhipuli2579@gmail.com>
2023-12-04 01:12:03 -0800

    finished legends, implemented routings, and implemented dynamic scaling of Y-axis (close-value) of the graph based on the stock

--- website/views.py
+++ website/views.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template, request, jsonify
 import requests #API Import
 import json, time, requests
 from datetime import datetime
+import numpy as np
 
 views = Blueprint('views', __name__)
 
@@ -61,11 +62,25 @@ def data(symbol):
 
 @views.route('/<symbol>')
 def graphPage(symbol):
+    val_arr = []
     filepath = f'CS_122_Final_Project/stockDataJsons/{symbol}_data.JSON'
     with open(filepath, 'r') as file:
         contents = json.load(file)
         keyList = contents.keys()
         data = contents
-
+        for date, values in data['Time Series (Daily)'].items():
+             if '4. close' in values:  # Check if '4. close' key exists in the inner dictionary
+                close_value = values['4. close']
+                val_arr.append(close_value)
+    
+    val_arr = np.array(val_arr)
+    val_arr = val_arr.astype(float)
+    min_val = np.min(val_arr)
+    max_val = np.max(val_arr)
+    # print(val_arr)
+    
+    # print(min_val)
+    # print(max_val)
     symbols = ['Select a stock', 'AAPL', 'AMZN', 'AVGO','BRK-B', 'GOOG', 'HD' , 'IBM', 'JNJ', 'JPM', 'LLY', 'MA', 'META', 'MSFT', 'NVDA', 'PG', 'TSLA','TSM','UNH', 'V', 'XOM' ] 
-    return render_template('graphs.html', symbols=symbols, keyList=keyList, data=data)
+
+    return render_template('graphs.html', symbols=symbols, keyList=keyList, data=data, min_val=min_val, max_val=max_val)
